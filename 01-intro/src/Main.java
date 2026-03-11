1d5c591d058b1ecbdbfe63f909a4bcc369224c5f
BrainWen <2229553982@qq.com>
2026-03-11 22:38:34 +0800

    3-11 Java基础格式以及注释

---
+++ 01-intro/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello!");
+        System.out.println(" World!");
+
+        // 这是一个注释
+
+        /*
+         * 这是多行注释
+         * 可以一次性注释很多行
+         */
+    }
+}
